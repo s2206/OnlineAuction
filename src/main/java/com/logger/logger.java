c0010bc30eb5d98b4b885d5c3f7c2b69c7177207
s2206 <62993642+s2206@users.noreply.github.com>
2020-05-02 16:12:34 +0530

    logger
    log4j

---
+++ src/main/java/com/logger/logger.java
@@ -0,0 +1,15 @@
+package com.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class logger {
+
+	private static final Logger logger = LoggerFactory.getLogger(logger.class);
+
+	public static void main(String[] args) {
+		logger.debug("test");
+
+	}
+
+}
